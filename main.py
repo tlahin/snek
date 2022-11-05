4a29a8b10d8821c712a317f4abcd393367fe5f66
tlahin <teemu.lahin@gmail.com>
2022-11-05 23:28:28 +0200

    removed images and their dir, removed image button class, added new menu buttons and a new class to create them

--- main.py
+++ main.py
@@ -60,17 +60,15 @@ fontsmall = pygame.font.SysFont('Arial', 40)
 fontbig = pygame.font.SysFont('Arial', 80)
 
 # Main menu buttons
-start_button = image_button(window_data.width / 2 - 150 / 2, window_data.height / 2, start_img)
-options_button = image_button(window_data.width / 2 - 150 / 2, window_data.height / 2 + 55, options_img)
-quit_button = image_button(window_data.width / 2 - 150 / 2, window_data.height / 2 + 110, quit_img)
+start_button = text_button(window_data.width / 2 - 100 / 2, window_data.height / 2 - 55, 100, 50, "START", pygame.font.SysFont(None, 50))
+options_button = text_button(window_data.width / 2 - 100 / 2, window_data.height / 2, 100, 50, "OPTIONS", pygame.font.SysFont(None, 50))
+quit_button = text_button(window_data.width / 2 - 100 / 2, window_data.height / 2 + 55, 100, 50, "QUIT", pygame.font.SysFont(None, 50))
 
 # Settings struck: (snek, background, food)
 colour_settings = create_colour_settings(pygame.Color('black'), ('gray'), ('red'))
 
 # Snek struct: (speed, block size, head cords, body cords)
 snek_data = create_snek_struct(10, 10, [510, 500], [[510, 500], [520, 500], [530, 500]])
-
-# Pause game loop until space is pressed
 
 running = True
 start = True
@@ -87,10 +85,10 @@ while running:
 	if start_button.draw(window_data.window):
 		reset_data(snek_data)
 		play(snek_data, colour_settings, window_data)
-
+	
 	if options_button.draw(window_data.window):
 		options(snek_data, colour_settings, window_data)
-        
+
 	if quit_button.draw(window_data.window):
 		pygame.quit()
 	
