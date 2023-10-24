b4f123d0da7ac2c160d81f49aa54461d231c93cb
tlahin <teemu.lahin@gmail.com>
2023-10-24 10:18:38 +0300

    restructuring functions and files (renders)

--- srcs/render.py
+++ srcs/render.py
@@ -1,7 +1,25 @@
 
 import pygame
 
-def render(snek_data, window_data, colour_settings, wall, snack, power_up):
+# Renders score at the bottom of the screen, updates it in real time
+def score_board(window_data, score, difficulty_settings):
+
+	# Text font
+	board_font = pygame.font.SysFont('Arial', 80)
+
+	# Content
+	score_surface = board_font.render('Points: ' + str(score), True, ('black'))
+	difficulty_surface = board_font.render('Difficulty: ' + str(difficulty_settings.difficulty), True, ('black'))
+
+	# Board rect
+	score_rect = pygame.Rect(5, 705, 800, 700)
+	difficulty_rect = pygame.Rect(600, 705, 500, 700)
+
+	# Draws the rect with the content
+	window_data.window.blit(score_surface, score_rect)
+	window_data.window.blit(difficulty_surface, difficulty_rect)
+
+def render(snek_data, window_data, difficulty_settings, colour_settings, wall, snack, power_up, exit_button, score):
 
 	# Rendering snake
 	for pos in snek_data.body:
@@ -16,3 +34,10 @@ def render(snek_data, window_data, colour_settings, wall, snack, power_up):
 	# Rendering wall
 	for pos in wall.cords:
 		pygame.draw.rect(window_data.window, colour_settings.wall_colour, pygame.Rect((pos[0], pos[1]), (wall.size, wall.size)))
+
+	# Breaks the loops and shows end screen when pressed
+	if exit_button.draw(window_data.window):
+		dead = True
+
+	# Displays the score and difficulty
+	score_board(window_data, score, difficulty_settings)
